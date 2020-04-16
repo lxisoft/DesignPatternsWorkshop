c30b8063cfb0e639a1c8d571b4996ed9e6c5ae26
sarangibalu <sarangibalu.a@lxisoft.com>
2020-04-17 05:28:15 +0530

    implemented State-dp

--- AnimalGame/src/com/sarangi/animalgame/game/DoneStateSBA.java
+++ AnimalGame/src/com/sarangi/animalgame/game/DoneStateSBA.java
@@ -54,9 +54,10 @@ public class DoneStateSBA implements StateSBA{
 	 */
 	@Override
 	public void publishingWinnerSBA() {
-		
+
+		System.out.println("\n\t\t\t-------------------- CONGRAGULATIONS ------------------------");
 		System.out.println("\n\t------------------------The Winner is -> "+gameSBA.getWinnerAnimalSBA()+" Name is ->"+gameSBA.getWinnerAnimalSBA().getName()+"---------------------------------");
-		
+		System.out.println("\n\t\t\t--------------------------- END --------------------------");
 		//gameSBA.setState(gameSBA.getTodoStateSBA());  
 		
 	}

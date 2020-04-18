45fda251df66baa9dda72b7c0e5d4ef84db5a0ee
sarangibalu <sarangibalu.a@lxisoft.com>
2020-04-19 04:45:53 +0530

    Implemented Composite and Iterator - Dp

---
+++ AnimalGame/src/com/sarangi/animalgame/zoo/ZooComponentSBA.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sarangi.animalgame.zoo;
+
+/**
+ * TODO Provide a detailed description here
+ * @author Owner
+ * ,
+ */
+public abstract class ZooComponentSBA {
+	
+	public void add(ZooComponentSBA zooComponentSBA){
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(ZooComponentSBA zooComponentSBA){
+		throw new UnsupportedOperationException();
+	}
+	
+	public ZooComponentSBA getChild(int i){
+		throw new UnsupportedOperationException();
+	}
+		
+	public void print(){
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getDescription() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getName() {
+		throw new UnsupportedOperationException();
+	}
+
+
+}
---
+++ AnimalGame/src/com/sarangi/animalgame/zoo/ZooWorldSBA.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sarangi.animalgame.zoo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * TODO Provide a detailed description here
+ * @author Owner
+ * ,
+ */
+public class ZooWorldSBA extends ZooComponentSBA{
+	
+	List<ZooComponentSBA> zooComponents = new ArrayList<ZooComponentSBA>();
+	
+	private String name;
+	private String description;
+		
+	
+	/**
+	 * @param name
+	 * @param description
+	 */
+	public ZooWorldSBA(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}	
+	
+	public void add(ZooComponentSBA zooComponent){
+		zooComponents.add(zooComponent) ;
+	}
+	
+	public void remove(ZooComponentSBA zooComponent){
+		zooComponents.remove(zooComponent) ;
+	}
+	
+	public ZooComponentSBA getChild(int i){
+		return (ZooComponentSBA)zooComponents.get(i);
+	}
+		
+	public void print(){
+		
+			System.out.println("\n\t"+getName()+" , "+getDescription());			
+			System.out.println("\n\t-----------------------------------------------------------------------");
+			
+			Iterator<ZooComponentSBA> iterator = zooComponents.iterator();
+			while (iterator.hasNext()) {
+				ZooComponentSBA zooComponent = (ZooComponentSBA)iterator.next();
+				zooComponent.print();
+				
+			}
+			
+		}
+
+}
